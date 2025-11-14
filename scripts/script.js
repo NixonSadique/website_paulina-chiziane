378304c8c80dc669ddb0c5a23d02a1b297ea8a15
NixonSadique <nixonsadique@gmail.com>
2025-11-14 21:19:04 +0200

    sidebar changes minor adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,30 @@
-const toogleSidebar = () => {
-    const nav = document.getElementById("navigator")
-    isHidden = nav.classList.contains("hidden")
+// const toogleSidebar = () => {
+//     const nav = document.getElementById("navigator")
+//     isHidden = nav.classList.contains("hidden")
 
-    if(isHidden){
-        nav.classList.remove("hidden")
+//     if(isHidden){
+//         nav.classList.remove("hidden")
+//         return;
+//     }
+
+//     nav.classList.add("hidden")
+// }
+
+
+const toogleSidebar = () => {
+    const nav = document.getElementById("navigator");
+    const openIcon = document.getElementById("open");
+    const closeIcon = document.getElementById("close");
+    const isCollapsed =  nav.classList.contains("collapsed");
+
+    if(isCollapsed){
+        nav.classList.remove("collapsed");
+        closeIcon.classList.remove("hidden")
+        openIcon.classList.add("hidden")
         return;
     }
 
-    nav.classList.add("hidden")
+    nav.classList.add("collapsed");
+    openIcon.classList.remove("hidden")
+    closeIcon.classList.add("hidden")
 }
